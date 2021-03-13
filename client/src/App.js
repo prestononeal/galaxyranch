3f41889d62a3d25fafb98ad39b83678552d89248
Preston O'Neal <prestononeal@gmail.com>
2021-03-12 20:59:29 -0800

    Add Docker and App code

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import PingComponent from './PingComponent';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Deploy React + Go to Heroku using Docker
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <PingComponent />
       </header>
     </div>
   );
